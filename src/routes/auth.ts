8660df8c84a99ccc778f1358119b079cd0fe9426
Paul Murray <44061707+Code4Wyatt@users.noreply.github.com>
2024-07-01 16:21:14 +0100

    implement error handler

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import { login, signUp } from '../controllers/auth'
+import { errorHandler } from '../error-handler'
 
 const authRoutes: Router = Router()
 
-authRoutes.post('/signup', signUp)
-authRoutes.post('/login', login)
+authRoutes.post('/signup', errorHandler(signUp))
+authRoutes.post('/login', errorHandler(login))
 
 export default authRoutes

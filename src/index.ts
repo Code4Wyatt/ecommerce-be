4d5fe9e86a4698ce0c9041686ce8613995f53e19
Paul Murray <165902178+PaulMurray@users.noreply.github.com>
2024-07-01 11:54:54 +0100

    task: setup base app structure

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Working')
+})
+
+app.listen(8000, () => console.log('Server running'))

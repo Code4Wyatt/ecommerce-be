1143f1182bdb45b2bc4e05abef414c4141599f86
Paul Murray <165902178+PaulMurray@users.noreply.github.com>
2024-07-01 13:55:52 +0100

    task: add secrets.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express'
+import { PORT } from './secrets'
 
 const app: Express = express()
 
@@ -6,4 +7,4 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Working')
 })
 
-app.listen(8000, () => console.log(`Server running`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))

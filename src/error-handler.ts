cfd0ed96f0115b39556b1cb98db5580212c38b10
Paul Murray <44061707+Code4Wyatt@users.noreply.github.com>
2024-07-02 12:52:58 +0100

    update error-handler.ts to manage zod errors

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express"
 import { ErrorCode, HttpException } from "./exceptions/root"
 import { InternalException } from "./exceptions/internal-exception"
+import { ZodError } from "zod"
+import { BadRequestsException } from "./exceptions/bad-requests"
 
 export const errorHandler = (method: Function) => {
     return async (req: Request, res: Response, next: NextFunction) => {
@@ -11,7 +13,11 @@ export const errorHandler = (method: Function) => {
             if (error instanceof HttpException) {
                 exception = error
             } else {
-                exception = new InternalException('Something went wrong.', error, ErrorCode.INTERNAL_EXCEPTION)
+                if (error instanceof ZodError) {
+                    exception = new BadRequestsException('Unprocessable entity.', ErrorCode.UNPROCESSABLE_ENTITY)
+                } else {
+                    exception = new InternalException('Something went wrong.', error, ErrorCode.INTERNAL_EXCEPTION)
+                }
             }
             next(exception)
         }
